399ee929a5554fbd4ffe628d210c01b639652cd6
Diogo Azedo <diogoazedo@gmail.com>
2020-08-12 20:32:31 -0300

    second chance

--- contact_process.php
+++ contact_process.php
@@ -1,36 +1,49 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "contato@advogadosbp.com.br"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+include_once(‘phpmailer.php’); //Chama o arquivo phpmailer.php com as funções para realizar o envio.
+
+//#########################################
+// Recebe as informações do formulário
+//#########################################
+
+$nome = $_POST[’name’];
+$email = $_POST[’email’];
+$assunto = $_POST[‘subject’];
+$mensagem = $_POST[‘message’];
+
+//#########################################
+// Dados da conta de e-mail que fará o envio
+//#########################################
+
+$smtp = new Smtp(“localhost”); //Endereço do SMTP, geralmente localhost.
+$smtp->user = “contato@advogadosbp.com.br”; //Conta de email
+$smtp->pass = “A(V65rfZM(N_”; //Senha da Conta de e-mail.
+$smtp->debug = false; //Somente para usuários avançados que desejam o log do envio para testes.
+
+//#########################################
+// Envio do formulário
+//#########################################
+
+$to = “contato@advogadosbp.com.br”; //Informe aqui o e-mail que deve receber a mensagem do formulário.
+$from = $email;
+$subject = “Contato – ” . $assunto;
+$msg = $mensagem;
+
+if (isset($_POST[‘submit’])) {
+if($nome && $email && $assunto && $mensagem) {
+if($smtp->Send($to, $from, $subject, $msg)){
+echo “<script>alert(‘Contato enviado!’);</script>”;
+echo “<script>window.location = ‘index.html’;</script>”; //Altere aqui para o endereço de sua página.
+exit;
+}
+}
+
+else {
+echo “<script>alert(‘Preencha todos os campos!’);</script>”;
+echo “<script>window.location = ‘index.html#contato’;</script>”; //Altere aqui para o endereço de seu formulário
+exit;
+}
+
+}
+
 ?>
-
-<!DOCTYPE html>
-<head>
-<title>Form submission</title>
-</head>
-<body>
-
-<form action="" method="post">
-First Name: <input type="text" name="first_name"><br>
-Last Name: <input type="text" name="last_name"><br>
-Email: <input type="text" name="email"><br>
-Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
-<input type="submit" name="submit" value="Submit">
-</form>
-
-</body>
-</html> 
