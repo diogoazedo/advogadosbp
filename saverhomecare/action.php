3add6d92d24b8f8684fc0002596cc0af44eb3f8c
Diogo Azedo <diogoazedo@gmail.com>
2020-09-28 20:44:20 -0300

    fixing form

---
+++ saverhomecare/action.php
@@ -0,0 +1,43 @@
+<?php
+$message = $_POST['message'];
+//pega os dados que foi digitado no ID name.
+
+$name = $_POST['name'];
+//pega os dados que foi digitado no ID email.
+
+$sender = $_POST['sender'];
+//pega os dados que foi digitado no ID sebject.
+
+$subject = "Saver Home Care: Novo report feito"
+$myEmail = "contato@advogadosbp.com.br"; //é necessário informar um e-mail do próprio domínio
+
+/*abaixo contém os dados que serão enviados para o email
+cadastrado para receber o formulário*/
+
+$corpo = "Formulário enviado\n";
+$corpo .= "Mensagem: " . $message . "\n";
+$corpo .= "Nome envolvidos: " . $name . "\n";
+$corpo .= "Quem enviou: " . $sender . "\n";
+
+
+$email_to = 'contato@advogadosbp.com.br';
+//não esqueça de substituir este email pelo seu.
+
+$status = mail($email_to, $subject, $corpo);
+//enviando o email.
+
+if ($status) {
+  echo "<script> 
+        alert('Mensagem enviada com sucesso!'); 
+        window.location='index.html';      
+  </script>";
+//mensagem de form enviado com sucesso.
+
+} else {
+  echo "<script> 
+        alert('Falha ao enviar a mensagem.'); 
+        window.location='https://advogadosbp.com.br/saverhomecare/canaldenuncia.html';   
+        </script>";
+//mensagem de erro no envio. 
+}
+?>
